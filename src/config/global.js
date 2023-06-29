56ccccf1d7157a5525adac151de5ec7b53992a10
Oscar Espitia <odespitia@sena.edu.co>
2023-06-28 21:42:48 -0500

    accesibilidad

--- src/config/global.js
+++ src/config/global.js
@@ -121,6 +121,11 @@ export default {
       },
       {
         icono: 'fas fa-download',
+        titulo: 'Descargar PDF',
+        download: 'downloads/CFA_010_224204.pdf',
+      },
+      {
+        icono: 'fas fa-download',
         titulo: 'Descargar material',
         download: 'downloads/material.zip',
       },
